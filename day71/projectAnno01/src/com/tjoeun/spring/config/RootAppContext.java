6dcd19b18ac891563cb3c2e0d1af653f6f0c1b80
YeonJuJu <uplay_98@naver.com>
2021-04-06 16:51:08 +0900

    day71

---
+++ day71/projectAnno01/src/com/tjoeun/spring/config/RootAppContext.java
@@ -0,0 +1,21 @@
+package com.tjoeun.spring.config;
+
+import org.springframework.context.annotation.Configuration;
+
+// Bean 을 정의하는 xml 파일 지정하기
+// 프로젝트에서 사용하는 Bean을 정의하는 클래스
+// (root-context.xml)
+@Configuration
+public class RootAppContext {
+	
+	
+	
+}
+
+
+
+
+
+
+
+
---
+++ day71/projectAnno01/src/com/tjoeun/spring/config/ServletAppContext.java
@@ -0,0 +1,60 @@
+package com.tjoeun.spring.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// import com.tjoeun.spring.mapper.MapperInterface;
+
+// Spring MVC 프로젝트에 관련된 설정을 하는 클래스
+// (servlet-context.xml)
+@Configuration
+// @Controller 어노테이션이 붙은 클래스를
+// Controller 로 등록함
+@EnableWebMvc // (<annotation-driven />)
+// Scan 할 Bean 들이 모여있는 package 를 지정함
+@ComponentScan(basePackages = {"com.tjoeun.spring.controller", "com.tjoeun.spring.bean"})
+
+public class ServletAppContext 
+  implements WebMvcConfigurer {
+	
+  /*
+    Controller 에서 return하는 문자열(경로)에
+    접두사, 접미사 설정하기 
+  */
+  @Override
+  public void configureViewResolvers(ViewResolverRegistry registry) {
+    WebMvcConfigurer.super.configureViewResolvers(registry);
+    registry.jsp("/WEB-INF/view/",".jsp");
+  }
+  
+  /*
+    프로젝트에서 사용하는 정적 파일 경로 설정하기
+    이미지, 사운드, 동영상, js, css 등 
+  */
+  @Override
+  public void addResourceHandlers
+    (ResourceHandlerRegistry registry) {
+    WebMvcConfigurer.super.addResourceHandlers(registry);
+    registry.addResourceHandler("/**").addResourceLocations("/resources/");
+  }
+  
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
